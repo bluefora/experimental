9341254d59076d1f0466546ba641730ac826e2c5
Derixithy <derixithy@users.noreply.github.com>
2025-03-30 11:41:18 +0200

    Fixes cleanup script

--- rootcopy/usr/lib/firstrun/cleanup.sh
+++ rootcopy/usr/lib/firstrun/cleanup.sh
@@ -2,16 +2,21 @@
 
 # Swap to signed
 if rpm-ostree status -b | grep ostree-unverified-registry; then
+    # notify user that we are upgrading
+
     # Rebase to signed image
     SIGN_URI=$(rpm-ostree status -b | grep -A1 "BootedDeployment:" | grep -v "BootedDeployment" | sed -E 's/.+ostree-unverified-registry:(.+)/ostree-image-signed:docker:\/\/\1/')
     echo "Signing installation - This will take a while"
     plymouth display-message --text="Signing installation - This will take a while" || true
     # shellcheck disable=SC2086
     rpm-ostree rebase $SIGN_URI
+
+    # Notify user that a reboot is necessary, maybe wait first for flatpak to finish!
 fi
 
 # remove flatpak remote
-if [[ flatpak remotes | grep fedora ]]; then
+flatpak remotes | grep fedora
+if [[ $? ]]; then
     flatpak remote-delete fedora --force
 fi
 
