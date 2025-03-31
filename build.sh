ace002d70eb7f324175a87efe7e9f8bc723b64a2
Derixithy <derixithy@users.noreply.github.com>
2025-03-31 10:00:53 +0200

    Fix build script calling

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -ouex pipefail
 
 git clone -b dev https://github.com/bluefora/build-workstation /tmp/build
 rsync -av --keep-dirlinks /tmp/build/rootcopy/* /
-bash /tmp/build.sh
+bash /tmp/build/build.sh
